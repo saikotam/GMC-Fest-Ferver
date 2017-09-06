24666d0acbde451f909d341a57f20b46e667938d
saikotam <saikotamraju54@gmail.com>
2017-09-06 19:28:43 +0530

    atlast pwa

--- site.js
+++ site.js
@@ -1,3 +1,9 @@
-navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
-  console.log('Excellent, registered with scope: ', registration.scope);
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,59 @@
-/** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+/*
+ *
+ *  Air Horner
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+// Version 0.57
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('airhorner').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/2.html',
+        '/3.html',
+          '/custom1.css',
+          '/custom2.css',
+          '/custom1.js',
+          '/custom2.js',
+          '/custom3.css',
+        '/basic.css',
+        '/basic.js',
+        '/4.html',
+          '/5.html',
+          '/jb.html',
+          '/team.png',
+          '/logo.png'
+          
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
